a7939c66615928110a39f1b6ce4941dc6530be9b
Ashwani <50752971+anshu00dps@users.noreply.github.com>
2025-05-24 11:18:09 +0000

    data type conversion

---
+++ 01_Basics/conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "Hitesh"
+let valueNumber = Number(score)
+
+console.log(valueNumber) //NaN: Not a Number 
+console.log(typeof(valueNumber))
+
+let isLoggedIn = "Hitesh"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn) // "" : flase, "Anshu" : true
+console.log(typeof(booleanIsLoggedIn))
+
+let val = 3;
+let negVal = -val
+
+console.log(negVal) // -3
+let str1 = "hello"
+let str2 = "Anshu"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" +2) // o/p: 12
+console.log(1+"2") // o/p: 12
+console.log("1" + 2+ 2) // o/p: 122
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter) // o/p: 101
