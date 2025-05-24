fad3dad7abec1f5532d1822f26e051733a3efe95
Ashwani <50752971+anshu00dps@users.noreply.github.com>
2025-05-24 11:45:59 +0000

    comparison in JS

---
+++ 01_Basics/comparison.js
@@ -0,0 +1,12 @@
+console.log(2>1) // op: true
+console.log(2>=1) // op: true
+console.log(2<1)    // op: false
+console.log(2 == 1) // op: false
+console.log(2 != 1) // op: true
+
+console.log("2" >5) // op: false. here type conversion is hapenning
+console.log("02" > 5) 
+
+console.log(null>=0)
+console.log(undefined >=0)
+console.log("2" == 2)
