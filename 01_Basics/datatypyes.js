6309cb7b6178c7404ed2577aea9c7ce0832bc3a7
Ashwani <50752971+anshu00dps@users.noreply.github.com>
2025-05-24 11:06:03 +0000

    datatypes

---
+++ 01_Basics/datatypyes.js
@@ -0,0 +1,10 @@
+"use strict"; // treat all JS code as newer version
+
+console.log("Anshu")
+
+let name = "Ashwani" //string type data
+let age  = 23   // int type
+let isLoggedIn = false // boolean type
+let state; //undefined type variable
+
+console.log(typeof(state))
