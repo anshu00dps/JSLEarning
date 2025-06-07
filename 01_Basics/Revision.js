889632f2b2b97039a93b0c1d3f7630dbe25e89d3
Ashwani <50752971+anshu00dps@users.noreply.github.com>
2025-06-07 12:11:25 +0000

    Revision

--- 01_Basics/Revision.js
+++ 01_Basics/Revision.js
@@ -2,5 +2,25 @@ let str = "1"
 //convert to binary, number 
 let binStr = Boolean(str)
 let numStr = Number(str)
+let num = 1
 
-console.table(str , typeof(str), binStr, typeof(binStr), numStr, typeof(numStr))
+// == : compare two variables
+// === : compare two variables irrespective of their datatype
+// if(str === num){
+//     console.log(false)
+// }else{
+//     console.log(true)
+// }
+
+// let sym1 = Symbol("desc")
+// let sym2 = Symbol("desc")
+
+// sym1 = Symbol("desc1")
+
+// object declaration (Non-primitive)
+myObj = {
+    name: "Ashwani",
+    age: 22
+}
+console.log(myObj.name)
+
